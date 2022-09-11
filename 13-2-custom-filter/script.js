cae3b51fbfeab142873f268c09214589689d55a9
SOunit <workbookwork7@gmail.com>
2022-09-11 13:33:48 -0700

    add memo

--- 13-2-custom-filter/script.js
+++ 13-2-custom-filter/script.js
@@ -1,8 +1,24 @@
 (function () {
   'use strict';
 
-  angular.module('app', []).controller('mainController', mainController);
+  angular
+    .module('app', [])
+    .controller('mainController', mainController)
+    // 2. register filter
+    // name is used to DI by Angular, getFive + Filter
+    .filter('getFive', getFiveFilter);
 
-  mainController.$inject = [];
-  function mainController() {}
+  // 3. get via DI
+  // follow name convention of Angular
+  mainController.$inject = ['$scope', 'getFiveFilter'];
+  function mainController($scope, getFiveFilter) {
+    $scope.name = getFiveFilter();
+  }
+
+  // 1. create filter factory
+  function getFiveFilter() {
+    return function () {
+      return 5;
+    };
+  }
 })();
